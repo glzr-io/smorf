465afe0eedf4b7a56fb69138e396439e6028c79b
Lars Berger <lars.berger@yahoo.com>
2024-10-08 18:59:25 +0800

    fix: remove usage of `Get` from `type-fest`; use own type instead

--- src/types/field-value.model.ts
+++ src/types/field-value.model.ts
@@ -1,7 +1,24 @@
-import type { Get } from 'type-fest/source/get';
-
 import type { FormValue } from './form-value.model';
 import type { FieldPath } from './field-path.model';
+
+type Idx<T, K extends string> = K extends keyof T
+  ? T[K]
+  : K extends `${number}`
+    ? number extends keyof T
+      ? T[number]
+      : never
+    : never;
+
+/**
+ * Gets the value at the stringified path to a key.
+ *
+ * Ref: https://stackoverflow.com/a/71097605/9139929
+ */
+type DeepIndex<T, K extends string> = T extends object
+  ? K extends `${infer F}.${infer R}`
+    ? DeepIndex<Idx<T, F>, R>
+    : Idx<T, K>
+  : never;
 
 /**
  * Type representing the value at given field path.
@@ -11,7 +28,7 @@ import type { FieldPath } from './field-path.model';
  * FieldValue<{ first: { name: 'homer' } }, 'first.name'>; // 'homer'
  * ```
  */
-export type FieldValue<V extends FormValue, P extends FieldPath<V>> = Get<
-  V,
-  P
->;
+export type FieldValue<
+  V extends FormValue,
+  P extends FieldPath<V>,
+> = DeepIndex<V, P>;
